bf470f3d7c5166e127eddd961f4917a78fefe4e6
Mark Bieganek <mark.bieganek@telus.com>
2022-11-24 16:50:11 -0700

    Describe fetchCoordsByIP function with comments

--- iss.js
+++ iss.js
@@ -9,9 +9,6 @@ const request = require('request');
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 const fetchMyIP = (callback) => {
-  // use request to fetch IP address from JSON API
-  // endpoint https://api.ipify.org?format=json
-  // return data as {"ip":"75.156.142.74"}
   const url = 'https://api.ipify.org?format=json';
   request(url, (error, response, body) => {
     if (error) {
@@ -31,12 +28,16 @@ const fetchMyIP = (callback) => {
   });
 };
 
+
+/**
+ * Makes a single API request to retrieve the user's longitude and longitude.
+ * Input:
+ *   - A callback (to pass back an error or the coordinates as an object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The coordinates (null if error). Example: { latitude: 37.3860517, longitude: -122.0838511 }
+ */
 const fetchCoordsByIP = (ip, callback) => {
-  // endpoint: http://ipwho.is/8.8.4.4
-  // JSON output:
-  /*
-  {"ip":"8.8.4.4","success":true,"type":"IPv4","continent":"North America","continent_code":"NA","country":"United States","country_code":"US","region":"California","region_code":"CA","city":"Mountain View","latitude":37.3860517,"longitude":-122.0838511,"is_eu":false,"postal":"94039","calling_code":"1","capital":"Washington D.C.","borders":"CA,MX","flag":{"img":"https:\/\/cdn.ipwhois.io\/flags\/us.svg","emoji":"\ud83c\uddfa\ud83c\uddf8","emoji_unicode":"U+1F1FA U+1F1F8"},"connection":{"asn":15169,"org":"Google LLC","isp":"Google LLC","domain":"google.com"},"timezone":{"id":"America\/Los_Angeles","abbr":"PST","is_dst":false,"offset":-28800,"utc":"-08:00","current_time":"2022-11-24T15:32:32-08:00"}}
-  */
   const url = 'https://ipwho.is/' + ip;
   request(url, (error, response, body) => {
     if (error) {
