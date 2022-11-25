04d71eeb875aed49a889de3e9d16ebc75a486957
Mark Bieganek <mark.bieganek@telus.com>
2022-11-24 19:19:31 -0700

    Return coordinates

--- index2.js
+++ index2.js
@@ -1,6 +1,8 @@
 const {
   fetchMyIP,
+  fetchCoordsByIP,
 } = require('./iss-promised');
 
 fetchMyIP()
+  .then(fetchCoordsByIP)
   .then(response => console.log(response));
--- iss-promised.js
+++ iss-promised.js
@@ -1,7 +1,20 @@
 const request = require('request-promise-native');
 
 const fetchMyIP = () => request('https://api.ipify.org?format=json');
+// const fetchCoordsByIP = (ip) => ;
+
+/* 
+ * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = (body) => {
+  const parsedJSON = JSON.parse(body);
+  const ip = parsedJSON.ip;
+  return request('https://ipwho.is/' + ip);
+};
 
 module.exports = {
   fetchMyIP,
+  fetchCoordsByIP,
 }
